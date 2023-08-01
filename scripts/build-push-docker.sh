5ec38907cd464c159cd0c2b09fecbfaa4dcc5eaa
saptak <dassaptak0@gmail.com>
2023-08-02 00:01:00 +0530

    :green_heart: Fixed CI pipeline

--- scripts/build-push-docker.sh
+++ scripts/build-push-docker.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 GCLOUD_SERVICE_IMAGE_NAME=$1
-TAG_NAME=$2
 GCLOUD_TMP_JSON_PATH="/tmp/gcloud-api-key.json"
 
 #? Login to Docker using the service account
 gcloud_docker_login(){
-    cat $GCLOUD_TMP_JSON_PATH | docker login -u _json_key --password-stdin https://${{ GCLOUD_DOCKER_REGISTRY_HOSTNAME }}
+    cat $GCLOUD_TMP_JSON_PATH | docker login -u _json_key --password-stdin https://${{ secrets.GCLOUD_DOCKER_REGISTRY_HOSTNAME }}
 }
 
 #? Set the image name and tag
