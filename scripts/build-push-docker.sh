986ee5fd6d2d1a4c7e5799f27127e5c94808926f
saptak <dassaptak0@gmail.com>
2023-08-02 00:17:38 +0530

    :green_heart: Modified CI pipeline

--- scripts/build-push-docker.sh
+++ scripts/build-push-docker.sh
@@ -3,15 +3,20 @@
 GCLOUD_SERVICE_IMAGE_NAME=$1
 GCLOUD_TMP_JSON_PATH="/tmp/gcloud-api-key.json"
 
+#? Decode the service account JSON from github secrets
+gcloud_decode_service_account (){
+    echo $GCLOUD_ENCODED_API_KEY | base64 -d > $GCLOUD_TMP_JSON_PATH
+}
+
 #? Login to Docker using the service account
 gcloud_docker_login(){
-    cat $GCLOUD_TMP_JSON_PATH | docker login -u _json_key --password-stdin https://${{ secrets.GCLOUD_DOCKER_REGISTRY_HOSTNAME }}
+    cat $GCLOUD_TMP_JSON_PATH | docker login -u _json_key --password-stdin https://$GCLOUD_DOCKER_REGISTRY_HOSTNAME
 }
 
 #? Set the image name and tag
 gcloud_set_image_tag() {
     export SERVICE_IMAGE_NAME=$GCLOUD_SERVICE_IMAGE_NAME
-    export GCR_IMAGE_NAME=${{ secrets.GCLOUD_DOCKER_REGISTRY_HOSTNAME }}/${{ secrets.GCLOUD_PROJECT_ID }}/${{ secrets.GCLOUD_DOCKER_REPO_NAME }}/$SERVICE_IMAGE_NAME
+    export GCR_IMAGE_NAME=$GCLOUD_DOCKER_REGISTRY_HOSTNAME/$GCLOUD_PROJECT_ID/$GCLOUD_DOCKER_REPO_NAME/$SERVICE_IMAGE_NAME
 }
 
 #? Build the docker image and set tag
