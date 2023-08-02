49fe85394c1c31b43eec169f2d144b4ec6085534
saptak <dassaptak0@gmail.com>
2023-08-02 21:07:42 +0530

    :greeen_heart: Added CD pipeline

--- scripts/gcloud-auth-login.sh
+++ scripts/gcloud-auth-login.sh
@@ -8,7 +8,7 @@ echo $GCLOUD_ENCODED_API_KEY| base64 -d > $GCLOUD_TMP_JSON_PATH
 #? Run gcloud auth
 gcloud info --run-diagnostics
 gcloud auth activate-service-account $GCLOUD_PROJECT_SERVICE_ACCOUNT_ID --key-file $GCLOUD_TMP_JSON_PATH
-gcloud config set project $GCLOUD_DEPLOYMENT_PROJECT_ID
+gcloud config set project $GCLOUD_PROJECT_ID
 
 #? Optional
 export GOOGLE_APPLICATION_CREDENTIALS=$GCLOUD_TMP_JSON_PATH
